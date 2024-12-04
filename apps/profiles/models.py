45ff95d32f922eda615a365071bddc046b77e031
alex <opar1297@mail.ru>
2024-12-04 17:51:20 +0300

    Модель адреса

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.accounts.models import User
+
+
+class ShippingAddress(BaseModel):
+    """
+    Модель адреса доставки пользователя
+
+    Атрибуты:
+        user (ForeignKey): Модель пользователя
+        full_name (str): Полное имя
+        email (str): Email
+        phone (str): Телефон
+        address (str): адрес
+        city (str): Город
+        country (str): Страна
+        zipcode (int): Почтовый индекс
+
+    Методы:
+        __str__: Возвращает информацию об адресе
+    """
+
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='shipping_addresses')
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    zipcode = models.IntegerField(null=True)
+
+    def __str__(self):
+        """
+        Возвращает информацию об адресе пользователя
+        """
+        return f'Адрес пользователя {self.full_name}'
